35ea91415d2ba206896736f5da4de500fe44a41b
Dmitriy Selivanov <selivanov.dmitriy@gmail.com>
2015-06-26 00:29:58 +0300

    rcpp exports bump

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -5,6 +5,18 @@
 
 using namespace Rcpp;
 
+// hashfun_1
+Rcpp::IntegerVector hashfun_1(IntegerVector vec, int cores);
+RcppExport SEXP LSHR_hashfun_1(SEXP vecSEXP, SEXP coresSEXP) {
+BEGIN_RCPP
+    Rcpp::RObject __result;
+    Rcpp::RNGScope __rngScope;
+    Rcpp::traits::input_parameter< IntegerVector >::type vec(vecSEXP);
+    Rcpp::traits::input_parameter< int >::type cores(coresSEXP);
+    __result = Rcpp::wrap(hashfun_1(vec, cores));
+    return __result;
+END_RCPP
+}
 // get_hash_matrix
 IntegerMatrix get_hash_matrix(int unique_shingles_length, int hashfun_number, int cores);
 RcppExport SEXP LSHR_get_hash_matrix(SEXP unique_shingles_lengthSEXP, SEXP hashfun_numberSEXP, SEXP coresSEXP) {
@@ -15,6 +27,28 @@ BEGIN_RCPP
     Rcpp::traits::input_parameter< int >::type hashfun_number(hashfun_numberSEXP);
     Rcpp::traits::input_parameter< int >::type cores(coresSEXP);
     __result = Rcpp::wrap(get_hash_matrix(unique_shingles_length, hashfun_number, cores));
+    return __result;
+END_RCPP
+}
+// get_tdm_character
+List get_tdm_character(List sets);
+RcppExport SEXP LSHR_get_tdm_character(SEXP setsSEXP) {
+BEGIN_RCPP
+    Rcpp::RObject __result;
+    Rcpp::RNGScope __rngScope;
+    Rcpp::traits::input_parameter< List >::type sets(setsSEXP);
+    __result = Rcpp::wrap(get_tdm_character(sets));
+    return __result;
+END_RCPP
+}
+// get_tdm_integer
+List get_tdm_integer(List sets);
+RcppExport SEXP LSHR_get_tdm_integer(SEXP setsSEXP) {
+BEGIN_RCPP
+    Rcpp::RObject __result;
+    Rcpp::RNGScope __rngScope;
+    Rcpp::traits::input_parameter< List >::type sets(setsSEXP);
+    __result = Rcpp::wrap(get_tdm_integer(sets));
     return __result;
 END_RCPP
 }
