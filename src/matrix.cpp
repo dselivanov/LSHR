67332680a383c6d57c70a995d18e39e8b5102c5b
Dmitriy Selivanov <selivanov.dmitriy@gmail.com>
2015-06-26 00:26:24 +0300

    tdm calculation for int and string sets using template

--- src/matrix.cpp
+++ src/matrix.cpp
@@ -1,28 +1,25 @@
-#include <Rcpp.h>
-#include <unordered_map>
-// Enable C++11
-// [[Rcpp::plugins(cpp11)]]
-using namespace Rcpp;
-using namespace std;
-
-// [[Rcpp::export]]
-vector< vector<int> >  get_tdm(List sets) {
-  vector< vector< int > > res( sets.size() );
-  unordered_map < string, int> dict;
-  unordered_map < string, int > :: const_iterator element_it;
+#include "lshr.h"
+//
+template<class T>
+List  get_tdm(List sets) {
+//vector< vector<int> >  get_tdm(List sets) {
+  //vector< vector< int > > res( sets.size() );
+  List res( sets.size() );
+  unordered_map < T, int> dict;
+  typename unordered_map < T, int > :: const_iterator element_it;
   int k = 0;
   for (auto it : sets) {
     checkUserInterrupt();
-    CharacterVector current_set = it;
+    vector<T> current_set = it;
     int K = current_set.size();
     int j = 0;
     vector< int > indices(K);
     for (auto element : current_set) {
-      element_it = dict.find(as<string>(element));
+      element_it = dict.find(element);
       int key;
       if(element_it == dict.end()) {
         key = dict.size() + 1;
-        dict.insert(make_pair(as<string>(element), key));
+        dict.insert(make_pair(element, key));
       } else {
         key = element_it -> second;
       }
@@ -32,7 +29,16 @@ vector< vector<int> >  get_tdm(List sets) {
     res[k] = indices;
     k++;
   }
-  return(res);
+  return res;
+}
+
+// [[Rcpp::export]]
+List  get_tdm_character(List sets) {
+  return get_tdm< string >(sets);
+}
+// [[Rcpp::export]]
+List  get_tdm_integer(List sets) {
+  return get_tdm< int >(sets);
 }
 
 // You can include R code blocks in C++ files processed with sourceCpp
@@ -40,6 +46,7 @@ vector< vector<int> >  get_tdm(List sets) {
 // run after the compilation.
 //
 
-/*** R
-get_tdm(list(letters[1:5], letters[3:7], letters[8:10]))
-*/
+// /*** R
+// get_tdm_character(list(letters[1:5], letters[3:7], letters[8:10]))
+// get_tdm_integer(list(1:5, 3:7, 8:11))
+// */
