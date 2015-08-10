237bb624de963e78474881c88895dc3e56269a22
Dmitry Selivanov <dmitry.selivanov@corp.mail.ru>
2015-08-10 19:28:21 +0300

    split lsh scheme into small functions

--- R/lsh.R
+++ R/lsh.R
@@ -1,18 +1,58 @@
+#'# @export
+create_index <- function(signature_matrix, bands_number, verbose = T) {
+  sm_nrow <- nrow(signature_matrix)
+  sm_ncol <- ncol(signature_matrix)
+  if( sm_nrow %% bands_number != 0)
+    stop("number of bands should be divisor of number of rows of signature matrix: 0 == nrow(signature_matrix) %% bands_number")
+  rows_per_band <- sm_nrow / bands_number
+  # calculate band borders for splitting signarure matrix
+  splits <- split_vector(x = seq_len(sm_nrow), splits = bands_number)
+  start <- Sys.time()
+  bands_hashes <- Map(hash_band, splits,
+                      MoreArgs = list(signature_matrix = signature_matrix ))
+  end <- Sys.time()
+  if(verbose)
+    print( paste( "hashing takes", difftime(end, start) ) )
+  start <- Sys.time()
+  # construct table for
+  index <- data.table(index = rep(seq_len(sm_ncol), bands_number),
+                      hash_value = unlist(bands_hashes, use.names = F),
+                      band_index = rep(seq_len(bands_number), each = sm_ncol),
+                      key = c('band_index', 'hash_value'))
+  end <- Sys.time()
+  if(verbose)
+    print( paste( "indexing takes", difftime(end, start) ) )
+  index
+}
+
+detect_candidate_pairs <- function(lsh_index, verbose) {
+  # look for candidates that have same hash value in a given band.
+  start <- Sys.time()
+  dt = lsh_index[ , list(index, .N), keyby = list(band_index, hash_value) ][N > 1, .(band_index, hash_value, index)]
+  # cross join to find candidate pair indices
+  dt = dt[dt, list(index1 = index, index2 = i.index), allow.cartesian = TRUE][index1 < index2]
+  # combine duplicated candidate pairs and get number of bands where band hashes are identical
+  res = dt[, .N, keyby = c('index1', 'index2')]
+  end <- Sys.time()
+  if(verbose)
+    print( paste( "detecting candidates takes", difftime(end, start) ) )
+  res
+}
+
 #' @export
 #' @name get_candidate_pairs
 #' @title Calculating candidate pairs using locality sensitive hashing.
 #'
 #' @param signature_matrix input signature matrix - \code{\link{integer}} \code{\link{matrix}}
 #' @param bands_number number of bands for LSH algorithm.
-#' @param similarity target value of jacard similarity we are looking for.
-#' @param verbose - \link{logical} print useful algorithm information.
+#' @param similarity target value of similarity we are looking for.
+#' @param verbose - \link{logical} print lsh process information.
+#' (such as expected false positive rate, false negative rate,timings, etc.)
 #' @return pairs of candidates with similarity => \code{similarity} -
 #' \code{\link{data.table}} with 3 colums: index1, index2, N -
 #' index of first candidate, index of second candidate,
-#' and number of buckets where they share same value. The latter provided
-#' only for information.
-#' (Intutition is following: the bigger N - the bigger similarity)
-#'
+#' and number of buckets where they share same value. The latter provided only for information.
+#' (Intutition is following: the bigger N - the stronger similarity)
 #'
 #' @examples
 #' sets <- lapply(1:10, function(x) sample(letters, sample(5:15)))
@@ -21,11 +61,11 @@
 #' sm <- get_signature_matrix(sets, 12, cores = 4)
 #' get_candidate_pairs(sm, 6, 0.9)
 get_candidate_pairs <- function(signature_matrix, bands_number, similarity, verbose = TRUE) {
-  # signature_matrix <- get_signature_matrix(sets, hashfun_number, cores = cores)
   sm_nrow <- nrow(signature_matrix)
   if( sm_nrow %% bands_number != 0)
     stop("number of bands should be divisor of number of rows of signature matrix: 0 == nrow(signature_matrix) %% bands_number")
   rows_per_band <- sm_nrow / bands_number
+
   if(verbose) {
     prob_become_candidate <- 1 - (1 - similarity ^ rows_per_band) ^ bands_number
     print(paste('Looking for sets with similarity',
@@ -34,36 +74,24 @@ get_candidate_pairs <- function(signature_matrix, bands_number, similarity, verb
                 prob_become_candidate))
   }
 
-  # calculate bands borders for splitting signarure matrix
-  splits <- split_vector(x = 1:sm_nrow, splits = bands_number)
-  bands_hashes <- Map(hash_band, splits,
-                 MoreArgs = list(signature_matrix = signature_matrix ))
-
-  candidate_pairs <- detect_candidate_pairs(bands_hashes)
-  candidate_pairs
+  # create hash tables - one per bucket
+  # lsh_index = sorted hash tables
+  lsh_index <- create_index(signature_matrix, bands_number, verbose)
+  detect_candidate_pairs(lsh_index, verbose)
 }
 
-detect_candidate_pairs <- function(band_hashes) {
-  bands_number <- length(band_hashes)
-  # all bands_hashes have equal row lengths
-  rows_per_band <- length(band_hashes[[1]])
-  # construct table for
-  dt <- data.table(index = rep(1:rows_per_band, bands_number),
-                   hash_value = unlist(band_hashes),
-                   band_index = rep(1:bands_number, each = rows_per_band),
-                   key = c('band_index', 'hash_value'))
-
-  # look for candidates that have same hash value in a given band.
-  dt = dt[ , list(index, .N), keyby = list(band_index, hash_value) ][N > 1, .(band_index, hash_value, index)]
-  # cross join to find candidate pair indices
-  dt = dt[dt, list(index1 = index, index2 = i.index), allow.cartesian = TRUE][index1 < index2]
-  # combine duplicated candidate pairs and get number of bands where band hashes are identical
-  dt[, .N, keyby = c('index1', 'index2')]#[N > 1]
-}
-
+#'# @export
 hash_band <- function(row_index_bounds, signature_matrix) {
+  MAX_INT = .Machine$integer.max
   row_indices <- row_index_bounds[[1]]:row_index_bounds[[2]]
-  # Using simple sum() as hash of signature chunk in each band.
-  # We can (and should) construct better hash function for hashing integer sequences
-  colSums(signature_matrix[row_indices, , drop = FALSE])
+  # hash each band -
+  # 1) multiply each value by its position
+  # 2) take mod MAX_INT to ensure that there is no int overflow.
+  # (overflow is very ok for us! but R cast to numeric when detect int overflow)
+  # don't know how to allow integer overflow in R. Suggestions are very welcome.
+  # 3) hash that value
+  temp <- hashfun_1( (signature_matrix[row_indices, , drop = FALSE] * as.numeric(seq_along(row_indices))) %% MAX_INT)
+  setattr(temp, 'dim', c(length(row_indices), ncol(signature_matrix) ))
+  as.integer(colSums(temp) %% MAX_INT)
+  # colSums(temp)
 }
