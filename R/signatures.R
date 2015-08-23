171e338f24a748f0692efffca147ba4c6f803bee
Dmitry Selivanov <selivanov.dmitriy@gmail.com>
2015-08-23 21:44:45 +0300

    minor refactoring and utils

--- R/signatures.R
+++ R/signatures.R
@@ -1,18 +1,28 @@
 #' @export
-get_hash_matrix <- function(nrow_hash_matrix, hashfun_number, measure, cores) {
+#' @name get_hash_matrix
+#' @title Calculating hashing matrix for given similarity measure.
+#' @param terms_size number of
+#' @param hashfun_number - number of projections to use for approximation. Involves
+#' tradeoff: larger is better, but computattionally more expensive and requires more RAM
+#' @param measure Similarity measure
+#' @param cores number of CPU cores to use for hashing. (related only for "jaccard" measure)
+#' @description For "jaccard" we generate construct hashing matrix
+#' which simulate to permutation of rows. For "cosine" we generate random
+#' hyperplanes.
+get_hash_matrix <- function(dtm, hashfun_number, measure, cores) {
+  terms_size <- get_terms_size(dtm)
   hash_matrix <-
     if(measure == 'jaccard') {
       # calculate hashes for each hash function and each row number
-      get_minhash_matrix(unique_shingles_length = nrow_hash_matrix,
+      get_minhash_matrix(unique_shingles_length = terms_size,
                          hashfun_number = hashfun_number,
                          cores = cores)
     } else if(measure == 'cosine') {
       # random_hyperpalnes
       matrix(sample(x = c (1L, -1L),
-                    size = nrow_hash_matrix * hashfun_number,
-                    # size = nrow(tdm_csr) * hashfun_number,
+                    size = terms_size * hashfun_number,
                     replace = T),
-             nrow_hash_matrix = hashfun_number)
+             ncol = hashfun_number)
     } else
       stop(paste(measure, "not supported"))
   hash_matrix
@@ -20,33 +30,31 @@ get_hash_matrix <- function(nrow_hash_matrix, hashfun_number, measure, cores) {
 
 #' @export
 #' @name get_signature_matrix
-#' @title Calculating signature matrix using minhashing techniqe.
+#' @title Calculating signature matrix for given similarity measure.
 #'
-#' @param tdm_lil - sparse input matrix in list of lists form: \code{\link{list}} of \code{\link{vector}}s
-#' It also may contain ncol attribute - number of columns.
-#' @param hashfun_number number of hash functions to calculate signature matrix.
-#' @param measure measure we want to calculate: \code{\link{character}}. Possible values - \code{c('jaccard', 'cosine')}
-#' @param cores number of CPU threads to use while calculating hashes
+#' @param dtm_lil - sparse document-term input matrix in list of lists form: \code{\link{list}} of \code{\link{vector}}s
+#' length(dtm_lil) = number of documents.
+#' It also may contain ncol attribute - number of terms.
+#' @param hashfun_number number of hash functions to use in construction of signature matrix.
+#' @param measure similarity measure we want to preserve in signature matrix: \code{\link{character}}.
+#' Possible values - \code{c('jaccard', 'cosine')}
+#' @param cores number of CPU threads to use while calculating hashes.
+#'Currently related only to "jaccard" measure. Uses OpenMP under the hood.
 #'
-#' @return The signature matrix - \code{\link{integer}} \code{\link{matrix}} with dimension \code{hashfun_number * length(tdm_lil)}
+#' @return The signature matrix - \code{\link{integer}} \code{\link{matrix}} with dimension \code{hashfun_number * length(dtm_lil)}
 #'
 #' @examples
 #' sets <- lapply(1:10, function(x) sample(letters, sample(5:15)))
-#' tdm_lil <- create_tdm(sets)
-#' sm <- get_signature_matrix(tdm_lil, hashfun_number = 10, measure = 'jaccard', cores = 4)
-get_signature_matrix <- function (tdm_lil, hashfun_number, measure = 'jaccard', cores = parallel::detectCores()) {
+#' dtm_lil <- create_dtm(sets)
+#' sm <- get_signature_matrix(dtm_lil, hashfun_number = 10, measure = 'jaccard', cores = 4)
+get_signature_matrix <- function (dtm, hashfun_number, measure = 'jaccard', cores = parallel::detectCores()) {
+  hash_matrix <- get_hash_matrix(dtm, hashfun_number, measure, cores)
 
-  nrow_hash_matrix <- attr(tdm_lil, 'ncol')
-  # if we don't know number of columns of tdm
-  if(is.null(nrow_hash_matrix))
-    nrow_hash_matrix <- unlist(tdm_lil, recursive = T, use.names = F) %>% unique %>% length
-
-  hash_matrix <- get_hash_matrix(nrow_hash_matrix, hashfun_number, measure, cores)
   if(measure == 'jaccard') {
-    return( minhashing(tdm_lil, hash_matrix) )
+    return( minhashing(dtm, hash_matrix) )
   }
   if(measure == 'cosine') {
-    return( sketching(tdm_lil, hash_matrix) )
+    return( sketching(dtm, hash_matrix) )
   }
   stop(paste(measure , 'is not supported'))
 }
--- R/utils.R
+++ R/utils.R
@@ -1,5 +1,3 @@
-# for debug
-#'@export
 split_vector <- function(x, splits) {
   if(! is.vector(x)) stop("x must be vector or list")
   if (length(x) < splits ) {
@@ -11,75 +9,17 @@ split_vector <- function(x, splits) {
   mapply(FUN = function(lower, upper) list(c(lower, upper)), knots[-length(knots)], knots[-1] - 1)
 }
 
-# for debug
-#'@export
-jaccard_atomic <- function(x, y) {
-  intrsct <- length(intersect(x, y))
-  intrsct / (length(x) + length(y) - intrsct)
-}
-
-# for debug
-#'@export
-cosine_atomic <- function(x, y) {
-  as.numeric(x %*% y / sqrt( sum(x^2) * sum(y^2) ))
-}
-
-#' @name cosine
-#' @title Fast cosine similarity calculation.
-#'
-#' @param m - sparse \code{\link{dgCMatrix}} or dense input matrix.
-#' \code{cosine} calculates all pair similarities between rows of this matrix.
-#' @examples
-#' i <- c(1,3:8, 1:6); j <- c(2,9,6:10,6:1); x <- c(6* (1:6), 7 * (1:7))
-#' m <- sparseMatrix(i, j, x = x)
-#' cosine(m)
-#'@export
-cosine <- function(m) {
-  # http://stats.stackexchange.com/a/81877/37081
-  if(inherits(m, 'Matrix'))
-    m =  as(m, 'dgCMatrix')
-  else if(!inherits(m, 'matrix'))
-    stop("m should be value of matrix or Matrix classes")
-  # normalize each row so it's vector distance is 1
-  row_norms <- sqrt(Matrix::rowSums(m^2))
-  row_norms <- t(crossprod(sign(m), Diagonal(x=row_norms)))
-  row_norms@x <- 1/row_norms@x
-  m_norm <- m * row_norms
-  tcrossprod(m_norm)
-}
-
-#' @name jaccard
-#' @title Fast jaccard similarity calculation.
-#'
-#' @param m - sparse \code{\link{dgCMatrix}} (or object that can be coerced to)
-#' or dense input matrix.
-#' \code{jaccard} calculates all pair similarities between rows of this matrix.
-#' @examples
-#' i <- c(1,3:8, 1:6); j <- c(2,9,6:10,6:1); x <- c(6* (1:6), 7 * (1:7))
-#' m <- sparseMatrix(i, j, x = x)
-#' jaccard(m)
-#'@export
-jaccard <- function(m) {
-  # http://stats.stackexchange.com/a/89947/37081
-  if(inherits(m, 'Matrix'))
-    m =  as(m, 'dgCMatrix')
-  else if(!inherits(m, 'matrix'))
-    stop("m should be value of matrix or Matrix classes")
-  ## common values:
-  A <- tcrossprod(m)
-  ## indexes for non-zero common values
-  im <- which(A > 0, arr.ind=TRUE, useNames = F)
-  ## counts for each row
-  b <- rowSums(m)
-
-  ## only non-zero values of common
-  Aim <- A[im]
-
-  ## Jacard formula: #common / (#i + #j - #common)
-  sparseMatrix(
-    i = im[,1],
-    j = im[,2],
-    x = Aim / (b[im[,1]] + b[im[,2]] - Aim),
-    dims = dim(A)
-  )
+get_terms_size <- function(dtm) {
+  # recieve sparse matrix in one on Matrix package formats (not in List-of-lists! form)
+  if(inherits(dtm, "Matrix")) {
+    terms_size <- ncol(dtm)
+  } else
+    # List-of-lists form
+  {
+    terms_size <- length(attr(dtm, 'terms'))
+    # if we don't know number of columns of lil dtm (it come not from LSHR::create_dtm)
+    if(is.null(terms_size))
+      terms_size <- unlist(dtm_lil, recursive = T, use.names = F) %>% unique %>% length
+  }
+  terms_size
 }
