2fa33a0cab5c56b5f82f730283342561f94ffafc
Dmitry Selivanov <selivanov.dmitriy@gmail.com>
2016-01-02 16:20:43 +0300

    update docs

--- R/signatures.R
+++ R/signatures.R
@@ -1,3 +1,4 @@
+#' @export
 #' @name get_hash_matrix
 #' @title Calculating hashing matrix for given similarity measure.
 #' @param hashfun_number - number of projections to use for approximation. Involves
@@ -29,25 +30,34 @@ get_hash_matrix <- function(dtm, hashfun_number, measure = c('jaccard', 'cosine'
 #' @name get_signature_matrix
 #' @title Calculating signature matrix for given similarity measure.
 #'
-#' @param dtm_lil - sparse document-term input matrix in list of lists form: \code{\link{list}} of \code{\link{vector}}s
+#' @param dtm_lil - sparse document-term input matrix in list of lists form:
+#' \code{\link{list}} of \code{\link{vector}}s
 #' length(dtm_lil) = number of documents.
 #' It also may contain ncol attribute - number of terms.
-#' @param hashfun_number number of hash functions to use in construction of signature matrix.
-#' @param measure similarity measure we want to preserve in signature matrix: \code{\link{character}}.
-#' Possible values - \code{c('jaccard', 'cosine')}
-#'Currently related only to "jaccard" measure. Uses OpenMP under the hood.
+#' @param hashfun_number number of hash functions to use in construction of
+#' signature matrix.
+#' @param measure similarity measure we want to preserve in signature matrix:
+#' \code{\link{character}}. Possible values - \code{c('jaccard', 'cosine')}
+#' @param ... parameters to \link{mclapply} function, which is used internally.
+#' For example you can provide \code{mc.cores = 4} to run minhashing
+#' algorithm on 4 cpu cores.
 #'
-#' @return The signature matrix - \code{\link{integer}} \code{\link{matrix}} with dimension \code{hashfun_number * length(dtm_lil)}
+#' @return The signature matrix - \code{\link{integer}} \code{\link{matrix}}
+#' with dimension \code{hashfun_number * length(dtm_lil)}
 #'
 #' @examples
 #' sets <- lapply(1:10, function(x) sample(letters, sample(5:15)))
 #' dtm_lil <- create_dtm(sets)
-#' sm <- get_signature_matrix(dtm_lil, hashfun_number = 10, measure = 'jaccard')
-get_signature_matrix <- function(dtm, hashfun_number, measure = c('jaccard', 'cosine'), seed = 0L) {
+#' sm <- get_signature_matrix(dtm_lil, hashfun_number = 10, measure = 'jaccard',
+#' mc.cores = 4)
+#'
+get_signature_matrix <- function(dtm, hashfun_number,
+                                 measure = c('jaccard', 'cosine'),
+                                 seed = 0L, ...) {
   measure <- match.arg(measure)
   hash_matrix <- get_hash_matrix(dtm, hashfun_number, measure, seed)
   switch(measure,
-          jaccard = minhashing(dtm, hash_matrix),
+          jaccard = minhashing(dtm, hash_matrix, ...),
           cosine = sketching(dtm, hash_matrix),
           stop(paste(measure, 'is not supported'))
           )
