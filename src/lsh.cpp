f1d5c954393f84a0f9df5f6d96da8432b33d8594
dselivanov <selivanov.dmitriy@gmail.com>
2017-08-16 15:06:09 +0400

    openmp flags + small cleanup

--- src/lsh.cpp
+++ src/lsh.cpp
@@ -1,17 +1,12 @@
 #include "LSHR.h"
 // [[Rcpp::export]]
 IntegerMatrix hash_signatures(IntegerMatrix m, int bands_number, int rows_per_band) {
-
-  // IntegerMatrix sketch_hashes(bands_number, m.ncol());
   IntegerMatrix sketch_hashes( m.ncol(), bands_number);
-
   for (int i = 0; i < m.ncol(); i++) {
     int index_row = 1;
     for(int j = 0; j < bands_number; j++) {
       int hash = 0;
       for (int k = 0; k < rows_per_band; k++) {
-        // sketch_hashes(j, i) =  atom_hashfun_1( m(j * rows_per_band + k, i) )  * (index_row + 1);
-        //hash +=  atom_hashfun_1( m(j * rows_per_band + k, i) )  * (index_row + 1);
         hash +=   atom_hashfun_1(m(j * rows_per_band + k, i)  * (index_row + 1));
         index_row++;
       }
